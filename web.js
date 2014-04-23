f846e6519dd729a3d98f6514673a41931c5a4f48
Xspirits <xspirits@gmail.com>
2014-04-23 16:25:13 +0200

    ahhhh

--- web.js
+++ web.js
@@ -3,8 +3,8 @@ var express = require('express');
 var compress = require('compression')();
 var app = express();
 
-app.use(express.static(__dirname + '/dist', { maxAge: 0 }));
 app.use(logger());
+app.use(express.static(__dirname + '/dist', { maxAge: 0 }));
 app.use(compress);
 app.listen(process.env.PORT || 5000);
 console.log("Listening on port: " + (process.env.PORT || 5000));
